63d5cd5ff90a9b6e023d72fbd8add7837742d3b6
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2022-09-24 19:30:29 +0200

    wrote main.py base code
    This is the base code for main.py. For the most part main.py should look like this in the entire program's lifecycle with minor changes here and there

---
+++ main.py
@@ -0,0 +1,89 @@
+# -- IMPORT FUNCTIONS FROM DEDICATED PYTHON FILES -- #
+from calculation import *
+from graph import *
+from solver import *
+
+# -- IMPORT MATPLOTLIB LIBRARY -- #
+from matplotlib import *
+
+print('>>>initializing program...')
+
+# -- VARIABLE TELLS THE PROGRAM WHAT MENU IT IS IN -- #
+current_menu = 'mm'
+
+# -- PRINT MAIN MENU ITEMS -- #
+def print_main_menu():
+    current_menu = 'mm'
+    return('\n-MAIN MENU- \n1. Calculation \n2. Graph \n3. Solver')
+
+# -- PRINT CALCULATION MENU ITEMS -- #
+def print_calculation_menu():
+    current_menu = 'calc'
+    print('\n- Calculation Mode')
+    return('Type your calculation below:')
+
+# -- PRINT GRAPH MENU ITEMS -- #
+def print_graph_menu():
+    current_menu = 'graph'
+    print('\n- Graph Mode')
+    return('Write graph equation below:')
+
+def print_solver_menu():
+    current_menu = 'solver'
+    print('\n- Solver Mode')
+    return('Write your equation below:')
+
+# -- ANALYZE USER INPUT -- #
+def take_input(userinput):
+    if userinput == 'home':
+        print_main_menu()
+    elif userinput == 'quit':
+        print('\n >>>Terminating program...')
+        quit()
+    else:
+        if current_menu == 'mm':
+            if userinput == '1':
+                print(print_calculation_menu())
+                user_in = input()
+                take_input(user_in)
+            elif userinput == '2':
+                print(print_graph_menu())
+                user_in = input()
+                take_input(user_in)
+            elif userinput == '3':
+                print(print_solver_menu())
+                user_in = input()
+                take_input(user_in)
+            else:
+                print('\nUndefined mode selected. Returning to Main Menu')
+                print_main_menu()
+                user_in = input()
+                take_input(user_in)
+        elif current_menu == 'calc':
+            user_in = input()
+            print('\n' + calcmenu_calculate(user_in))
+            user_in = input()
+            take_input(user_in)
+        elif current_menu == 'graph':
+            user_in = input()
+            print('\n' + graphmenu_graph(user_in))
+            user_in = input()
+            take_input(user_in)
+        elif current_menu == 'solver':
+            user_in = input()
+            print('\n' + solvermenu_solve(user_in))
+            user_in = input()
+            take_input(user_in)
+        else:
+            print('An unknown error has occured, and Maryam had to terminate. Sorry for the inconvenience')
+            print('\n >>>Terminating program...')
+            quit()
+            
+            
+print('\nWelcome to Maryam, created by Parsa Farjam \nCurrent Version: 0.0.0 ')
+
+# -- LOOP TERMINATES WHEN USER ENTERS QUIT COMMAND -- #
+while True:
+    print(print_main_menu())
+    user_in = input()
+    take_input(user_in)
